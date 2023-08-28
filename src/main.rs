d99c4378357b5c69e3793dd078f4c11cd59744d3
MQ-9 Reaper <taj.vanderklauw@student.education.wa.edu.au>
2023-08-28 15:10:53 +0800

    what?!
    Turns out I had already done it before I had started working on this

--- src/main.rs
+++ src/main.rs
@@ -266,33 +266,8 @@ fn compile_markdown (
 
                                         match key {
                                             "title" => {
-                                                let mut chars = val.chars();
-
-                                                // Get the first character
-                                                let first_char = chars.next(); // Returns an Option<char>
-                                            
-                                                // Iterate to the last character
-                                                let mut last_char = None;
-                                                while let Some(c) = chars.next() {
-                                                    last_char = Some(c);
-                                                }
-                                                if first_char == "\'".chars().next() || first_char == "\'".chars().next() {
-                                                    if last_char == "\"".chars().next() || last_char == "\"".chars().next() {
-                                                        if first_char == last_char {
-                                                            let mut modified_string: String = chars.collect();
-                                                            // Remove the first character
-                                                            modified_string.remove(0);
-
-                                                            // Remove the last character
-                                                            modified_string.pop();
-                                                            frontmatter.title = modified_string;
-                                                        }
-
-                                                        
-                                                    }
-                                                }
-                                               
-                                                
+                                                let processed_title = val.trim().trim_matches('\'').trim_matches('\"').to_string();
+                                                frontmatter.title = Some(processed_title);
                                             },
                                             "description" => {
                                                 frontmatter.description = Some(val.trim().trim_matches('\"').to_string())
